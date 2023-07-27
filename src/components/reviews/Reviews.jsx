058edcbc8f5fd73fbca6dfd2a55813d1f3200016
AleDC27 <walteralejandrogordillo@gmail.com>
2023-07-27 16:58:19 -0300

    bbb

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -2,24 +2,31 @@ import React from "react";
 import s from "./reviews.module.css";
 import { useState } from "react";
 
-export default function Reviews({ num }) {
+export default function Reviews() {
   const [send, setSend] = useState(true);
+  const [caracteres,setCaracteres]=useState(0);
 
   const handleClikc = () => {
     setSend(!send)
   };
+
+  const handleChange=(e)=>{
+    const inputText = e.target.value;
+    setCaracteres(inputText.length);
+  }
 
   return (
     <>
       {send ? (
         <div className={s.containerd}>
           <b>Danos tu opinión</b>
-          <span className={s.caracteres}>{num ? num : 0}/240</span>
+          <span className={s.caracteres}>{caracteres}/240</span>
           <br />
           <textarea
             maxLength="100"
             id="miTextarea"
             placeholder="¿Te gustá nuestra forma de hacer las cosas?"
+            onChange={handleChange}
           ></textarea>
           <br />
           <button className={s.send} onClick={handleClikc}>Enviar</button>
